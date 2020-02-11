9b3a8611c088b30616d1321f47e7049e10136812
ab-bisht <18abhishek.b@gmail.com>
2020-02-11 10:59:20 +0530

    remove header from landing page

--- layouts/default.vue
+++ layouts/default.vue
@@ -21,15 +21,8 @@ export default {
   },
   methods: {
     isHomeroute() {
-      this.isHome =
-        this.$route.params.city ||
-        this.$route.path === '/contact-us' ||
-        this.$route.path === '/about' ||
-        this.$route.path === '/privacy' ||
-        this.$route.path === '/careers' ||
-        this.$route.path === '/terms-of-use' ||
-        this.$route.path === '/community-guidelines'
-      return this.isHome
+      this.isHome = this.$route.name === 'index'
+      return !this.isHome
     }
   }
 }
