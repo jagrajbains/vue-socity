0799da5670362d99c575edf1c0c55dbea33e8b1a
ab-bisht <18abhishek.b@gmail.com>
2020-02-19 12:09:09 +0530

    privacy page added

--- pages/privacy/index.vue
+++ pages/privacy/index.vue
@@ -1,5 +1,142 @@
 <template>
   <div>
-    <h1>Privacy</h1>
+    <div class="privacy-container">
+      <h1 class="privacy-container-title">
+        So - A Local's City Guide Privacy Policy
+      </h1>
+      <span class="privacy-container-description-1">
+        So - A Local’s City Guide (so.city / Android & iOS app) recognises the
+        importance of maintaining your privacy. We value your privacy and
+        appreciate your trust in us. This Policy describes how we treat user
+        information we collect on so.city website as well as our Android & iOS
+        app and other offline sources. This Privacy Policy applies to current
+        and former visitors to our website and to our online visitors. By
+        visiting and/or using our website, you agree to this Privacy Policy. So
+        - A Local’s City Guide is a property of Acropole Travels Private
+        Limited, an Indian Company registered under the Companies Act, 2013
+        having its registered office at Gulmohar Park, New Delhi, Delhi - 110
+        049.
+      </span>
+      <span class="privacy-container-description ">
+        Information we collect Contact information. We might collect your name,
+        email, mobile number, phone number, street, city, state, pincode,
+        country and ip address. Information you post. We collect information you
+        post in a public space on our website or on a third-party social media
+        site belonging to so.city Demographic information. We may collect
+        demographic information about you, events you like, events you intend to
+        participate in or any other information provided by your during the use
+        of our website. We might collect this as a part of a survey also.
+        <br />Other information. If you use our website, we may collect
+        information about your IP address and the browser you're using. We might
+        look at what site you came from, duration of time spent on our website,
+        pages accessed or what site you visit when you leave us. We might also
+        collect the type of mobile device you are using, or the version of the
+        operating system your computer or device is running. We collect
+        information in different ways. We collect information directly from you.
+        We collect information directly from you when you perform certain
+        actions on the website or apps. We also collect information if you post
+        a comment on our websites or ask us a question through phone or email.
+        We collect information from you passively. We use tracking tools like
+        Google Analytics, Google Webmaster, browser cookies and web beacons for
+        collecting information about your usage of our website. We get
+        information about you from third parties. <br />
+        For example, if you use an integrated social media feature on our
+        websites. The third-party social media site will give us certain
+        information about you. This could include your name and email address.
+        Use of your personal information We use information to contact you: We
+        might use the information you provide to give you better matching feeds
+        and articles that you may like in accordance to what you have read
+        earlier. We use information to improve our products and services. We
+        might use your information to customize your experience with us. This
+        could include displaying content based upon your preferences. We use
+        information to look at site trends and customer interests. We may use
+        your information to make our website and products better. We may combine
+        information we get from you with information about you we get from third
+        parties. We use information for security purposes. We may use
+        information to protect our company, our customers, or our websites. We
+        use information for marketing purposes. We might send you information
+        about special promotions or offers. We might also tell you about new
+        features or products. These might be our own offers or products, or
+        third-party offers or products we think you might find interesting. Or,
+        for example, if you sign up on the platform with us we'll enroll you in
+        our newsletter. We use information to send you transactional
+        communications. We might send you emails or SMS about your account. We
+        use information as otherwise permitted by law. Sharing of information
+        with third-parties We will share information with the event organizers.
+        We share your information with event organizers and other parties
+        responsible for fulfilling the purchase obligation. The event organizers
+        and other parties may use the information we give them as described in
+        their privacy policies. We will share information with our business
+        partners. This includes a third party who provide or sponsor an event,
+        or who operates a venue where we hold events. Our partners use the
+        information we give them as described in their privacy policies. We may
+        share information if we think we have to in order to comply with the law
+        or to protect ourselves. We will share information to respond to a court
+        order or subpoena. We may also share it if a government agency or
+        investigatory body requests.<br />Or, we might also share information
+        when we are investigating potential fraud. We may share information with
+        any successor to all or part of our business. For example, if part of
+        our business is sold we may give our customer list as part of that
+        transaction. We may share your information for reasons not described in
+        this policy. We will tell you before we do this Email out Third party
+        sites If you click on one of the links to third party websites, you may
+        be taken to websites we do not control. This policy does not apply to
+        the privacy practices of those websites. Read the privacy policy of
+        other websites carefully. We are not responsible for these third party
+        sites. If you have any questions about this Policy or other privacy
+        concerns, you can also email us at [email protected] Updates to this
+        policy This Privacy Policy was last updated on 15.03.2017. From time to
+        time we may change our privacy practices. We will notify you of any
+        material changes to this policy as required by law. We will also post an
+        updated copy on our website. Please check our site periodically for
+        updates. Jurisdiction If you choose to visit the website, your visit and
+        any dispute over privacy is subject to this Policy and the website's
+        terms of use. In addition to the foregoing, any disputes arising under
+        this Policy shall be governed by the laws of India.
+      </span>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.privacy-container {
+  margin: 0px 8rem 0;
+  overflow: auto;
+}
+
+.privacy-container-title {
+  margin-top: 40px;
+  font-family: Lato, sans-serif;
+  font-weight: 400;
+}
+body.dark-theme .privacy-container-title {
+  color: white;
+}
+
+.privacy-container-description-1 {
+  margin-bottom: 40px;
+  font-family: Lato, sans-serif;
+}
+body.dark-theme .privacy-container-description-1 {
+  color: white;
+}
+
+.privacy-container-description {
+  font-family: Lato, sans-serif;
+}
+body.dark-theme .privacy-container-description {
+  color: white;
+}
+
+@media screen and (max-width: 768px) {
+  .privacy-container {
+    margin: 60px 2rem 0;
+    margin-top: 0px;
+  }
+
+  .privacy-container-title {
+    font-size: 20px;
+    font-weight: 500;
+  }
+}
+</style>
